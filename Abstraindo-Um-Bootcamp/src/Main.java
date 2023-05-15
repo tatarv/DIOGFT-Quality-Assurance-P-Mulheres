7abcdad87b7dbc478bd22444fc67c5551bfac18d
Tais <taisnayara145@hotmail.com>
2023-05-15 13:55:46 -0400

    Repositório do terceiro desafio

---
+++ Abstraindo-Um-Bootcamp/src/Main.java
@@ -0,0 +1,29 @@
+import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Mentoria;
+
+import java.time.LocalDate;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+       Curso curso1 = new Curso();
+       curso1.setTitulo("curso java");
+       curso1.setDescricao("descrição do curso java");
+       curso1.setCargaHoraria(8);
+
+        Curso curso2 = new Curso();
+        curso2.setTitulo("curso js");
+        curso2.setDescricao("descrição do curso js");
+        curso2.setCargaHoraria(4);
+
+        Mentoria mentoria = new Mentoria();
+        mentoria.setTitulo("mentoria de java");
+        mentoria.setDescricao("descrição mentoria java");
+        mentoria.setData(LocalDate.now());
+
+        System.out.println(curso1);
+        System.out.println(curso2);
+        System.out.println(mentoria);
+        }
+    }
